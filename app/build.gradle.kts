850dc49ad9e50e1126cc34328a16843c95b0045c
MC <8493007+mcnaveen@users.noreply.github.com>
2026-05-14 08:23:16 +0530

    chore: bump app version patch to 1.9.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val appVersionMajor = 1
 val appVersionMinor = 9
-val appVersionPatch = 4
+val appVersionPatch = 5
 val appVersionCode = (appVersionMajor * 10000) + (appVersionMinor * 100) + appVersionPatch
 val appVersionName = "$appVersionMajor.$appVersionMinor.$appVersionPatch"
 
